0c7658fa25e62be45ff7dc01639a9819bac9f5b5
Adlinnur Natasha <adlinnurnatasha@gmail.com>
2022-03-07 12:10:29 +0800

    Live Graph Server

--- src/pages/LiveGraphPage.js
+++ src/pages/LiveGraphPage.js
@@ -2,8 +2,7 @@ import React from "react";
 import StockPrice from "../components/LiveGraph";
 
 function LiveGraphPage() {
-
-    //state for symbol,interval
+  //state for symbol,interval
   return <StockPrice />;
 }
 
